e6bb01e4448cae288cc241fe4f627d9ec03a84cf
Unknown <kdeppmeier@yahoo.com>
2018-01-28 14:14:29 -0800

    Started MainMenu and SpriteFlicker

---
+++ Assets/Scripts/SpriteFlicker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteFlicker : MonoBehaviour
+{
+    public float waitTime;
+
+    private IEnumerator coroutine;
+
+    void Awake()
+    {
+        coroutine = Flicker(waitTime);
+        StartCoroutine(coroutine);
+    }
+
+    private IEnumerator Flicker(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+    }
+}

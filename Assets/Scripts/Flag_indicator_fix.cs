30c5a1061e033a02811b1adee0e045b5300da62f
Kent Pluntze <kent.pluntze@gmail.com>
2018-01-28 10:19:27 -0800

    Arrow Script

--- Assets/Scripts/Flag_indicator_fix.cs
+++ Assets/Scripts/Flag_indicator_fix.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class Flag_indicator_fix : MonoBehaviour {
 	Vector3 direction;
-	Quaternion spin;
-	// Use this for initialization
+    Vector3 dir;
+    float angle;
+    private GameObject destination;
+    // Use this for initialization
 	void Start (){
 	}
 
 	void FixedUpdate(){
 		direction= transform.parent.transform.position;
-		transform.position = new Vector3(direction.x, direction.y, direction.z+4);
-		transform.eulerAngles = new Vector3 (70, 0, 0);
-		}
+		transform.position = new Vector3(direction.x, direction.y+6, direction.z);
+
+        destination = GameObject.FindWithTag("Destination");
+        if (destination != null)
+        {
+            dir = destination.transform.position - transform.position;
+            angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+	}
 	// Update is called once per frame
 	void Update () {
 		
